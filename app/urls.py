70916cabb1ebc0baa93643ac9f20c9050b9b7f3c
Mo17 <mo.err17@gmail.com>
2021-04-16 21:54:45 +0200

    add new view functions

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, static_template
 
 app_name = 'app'
 
 
 urlpatterns = [
   path('', index),
+
+  # static urls
+  path('privacy-policy/', static_template('privacy.html'), name='privacy-policy'),
+  path('about-us/', static_template('aboutus.html'), name='about-us'),
+  path('contact-us/', static_template('contactus.html'), name='contact-us'),
 ]
