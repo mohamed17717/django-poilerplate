37ad9d37d37808f6e7fac709a893b29d0ce8e918
Mo17 <mo.err17@gmail.com>
2021-08-04 02:51:38 +0200

    add new feature (login system)

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, static_template
+from .views import (
+  index,
+  register_account,
+  login_account,
+  logout_account,
+  static_template
+)
 
 app_name = 'app'
 
@@ -9,6 +15,9 @@ app_name = 'app'
 urlpatterns = [
   path('', index),
 
+  path('register/', register_account.as_view(), name='register'),
+  path('login/', login_account.as_view(), name='login'),
+  path('logout/', logout_account, name='logout'),
   # static urls
   path('privacy-policy/', static_template('privacy.html'), name='privacy-policy'),
   path('about-us/', static_template('aboutus.html'), name='about-us'),
